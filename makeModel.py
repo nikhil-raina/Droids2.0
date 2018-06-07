5d877e8e80516d7ad985d123c4cf536b57adee27
Nikhil Raina <nxr5013@g.rit.edu>
2018-06-07 15:52:33 -0400

    Data Fetch procedure works completely fine. Need ro work on modelling.

--- makeModel.py
+++ makeModel.py
@@ -1,5 +1,16 @@
 import dataFetch as data
 
 if __name__ == '__main__':
-    labelList = 'CA'
-    atomLabel_withList = data.getData("CFTRdata\\atomflux_CFTRmutation\\DROIDSfluctuation_1.txt", labelList)
+    labelList = 'CA O N'
+    atomLabel_withList, missingLabels = data.getData("CFTRdata\\atomflux_CFTRmutation\\DROIDSfluctuation_1.txt", labelList)
+
+    for key, val in atomLabel_withList.items():
+        print(key, ':', val)
+
+    print()
+    print('The following atoms are missing from the dictionary:')
+    if missingLabels.__len__() == 0:
+        print('0')
+    else:
+        for labels in missingLabels:
+            print(labels)
